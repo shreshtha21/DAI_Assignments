5764c174ddbb91a70edac4253d68809a99659ea1
Shreshtha <shreshthagupta2111@gmail.com>
2025-08-15 17:44:21 +0530

    q2 updated

--- q2.m
+++ q2.m
@@ -29,7 +29,6 @@ end
 %function to calculate new standard deviation
 function newStd = UpdateStd (OldMean, OldStd, NewMean, NewDataValue, n)
     oldsumsquare=(n-1)*OldStd^2 + n*OldMean^2;
-    fprintf("%.4f",oldsumsquare)
     newvariance=(oldsumsquare + (NewDataValue^2) - (n+1)*NewMean^2)/n;
     newStd = sqrt(newvariance);
 end
